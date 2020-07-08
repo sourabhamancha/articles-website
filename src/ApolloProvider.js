c4d99c5d5ebb549b17b36244b32a1355bb199638
sourabhamancha <sourabh.amancha@gmail.com>
2020-07-08 16:11:39 +0530

    updated proxy and server base uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://arcane-ravine-34920.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
